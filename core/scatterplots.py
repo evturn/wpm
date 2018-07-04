0a69677ec1fa46cb316b54f2ad808f503caa4fa8
Evan Turner <ev@evturn.com>
2018-07-03 22:23:22 -0400

    Revise scatterplot colors and values

--- core/scatterplots.py
+++ core/scatterplots.py
@@ -4,11 +4,10 @@ fields = ['label', 'color', 'alpha', 'shape', 'edge']
 Scatterplot = namedtuple('Scatterplot', fields)
 
 scatterplots = [
-    Scatterplot('100%', 'navy', .8, 250, 'none'),
-    Scatterplot('99%', 'blue', .7, 175, 'none'),
-    Scatterplot('98%', 'dodgerblue', .5, 175, 'none'),
-    Scatterplot('97%', 'palegreen', .5, 125, 'none'),
-    Scatterplot('96%', 'lime', .7, 175, 'none'),
-    Scatterplot('95%', 'gold', .7, 175, 'navy'),
-    Scatterplot('94%', 'yellow', .8, 225, 'black')
+    Scatterplot('100%', 'midnightblue', .9, 400, 'none'),
+    Scatterplot('99%', 'dodgerblue', .9, 150, 'none'),
+    Scatterplot('98%', 'cyan', .6, 100, 'none'),
+    Scatterplot('97%', 'lime', .6, 100, 'none'),
+    Scatterplot('96%', 'yellow', .9, 250, 'none'),
+    Scatterplot('☠︎☠︎☠︎', 'red', .9, 350, 'none'),
 ]
